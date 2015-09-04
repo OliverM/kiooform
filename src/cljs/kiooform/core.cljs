f2a0410f18dc2b1f588ec65e2e10d805ba3297a5
Oliver Mooney <oliver.mooney@gmail.com>
2015-09-04 19:55:11 +0100

    Experimenting to see if I can pin down what's happening - added aprint and hickory to project.clj - used hickory to generate hiccup from the html template & pass to reagent - no issues, but I don't know if Kioo uses hickory in this mode anyway, it seems not?

--- src/cljs/kiooform/core.cljs
+++ src/cljs/kiooform/core.cljs
@@ -1,7 +1,16 @@
 (ns kiooform.core
     (:require [kioo.reagent :as k]
-              [reagent.core :as reagent :refer [atom]])
+              [reagent.core :as reagent :refer [atom]]
+              [hickory.core :as h])
     (:require-macros [kioo.reagent :refer [defsnippet deftemplate]]))
+
+;; try parsing html via hickory then inserting into reagent component
+(defn h-test []
+  (->> "<div class=\"outcomes\">\n    <h2>Outcomes</h2>\n    <form>\n        <fieldset>\n            <legend>Outcomes assessment</legend>\n            <div>\n                <div>\n                    <label for=\"outcomescompletiondate\">Outcomes for month ending</label>\n                    <input id=\"outcomescompletiondate\" type=\"date\">\n                </div>\n            </div>\n        </fieldset>\n    </form>\n</div>"
+      h/parse-fragment
+      (map h/as-hiccup)
+       first
+       ))
 
 ;; problematic kioo-generated component takes a long time to appear in the page
 (deftemplate minform "minform.html" [])
@@ -15,7 +24,8 @@
     [:span {:style {:color "red"}} "Red."]]])
 
 (defn mount-root []
-  (reagent/render-component [minform] (.getElementById js/document "app")))
+  (reagent/render-component [h-test]
+                            (.getElementById js/document "app")))
 
 (defn init! []
   (mount-root))
